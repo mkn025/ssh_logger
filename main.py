82ae6a58a2f42ebcd15202d84a18373ee72b6f1e
mkn025 <78483320+mkn025@users.noreply.github.com>
2023-09-20 21:07:48 +0200

    main

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from funksjoner import *
 import time
 
-log_tester = None
+logg_tester = None
 
 while True:
     try:
-        oppdateringer = int(hent_linjer("etc.conf",1,True))
-        antall_linjer = int(hent_linjer("etc.conf",3,True))
-        print(f"program kjører med {oppdateringer} sek og skrver {antall_linjer} linjer til fil")
+        updates = int(hent_linjer("conf/etc.conf",1,True))
+        amount_of_lines = int(hent_linjer("conf/etc.conf",3,True))
+        print(f"program updates every {updates} sek og writes lines {amount_of_lines} to file")
     except:
-        oppdateringer = 5
-        antall_linjer = 1
-        print("fant ikke fil")
-    log = hent_linjer("/var/log/auth.log", antall_linjer)
-    if log == log_tester:
+        updates = 5
+        amount_of_lines = 1
+        print("etc.conf was not found")
+    logg = hent_linjer("/var/log/auth.log", amount_of_lines)
+    if logg == logg_tester:
         pass
     else:
-        for n in log:
-            append_to_file("logg.txt",n)
+        for n in logg:
+            append_to_file("logg/logg.txt",n)
             ip_adresse = find_ip_addresses(n)
-            append_to_file("logg_ip.txt",f"ip adresse: {ip_adresse[0]}") if len(ip_adresse) > 0 else None
-    log_tester = log
-    time.sleep(oppdateringer)
+            append_to_file("logg/logg_ip.txt",f"ip adresse: {ip_adresse[0]}") if len(ip_adresse) > 0 else None
+    logg_tester = logg
+    time.sleep(updates)
 
 
 
