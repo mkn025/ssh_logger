a99aa0e8fbdf07d54bc6ea1d9cd0e9cb2e0f08da
mkn025 <martin.elde.knutsen@gmail.com>
2023-10-02 22:14:21 +0200

    legger til start time i programmet

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from funksjoner import *
 import time
 
 logg_tester = None
-
+antall_ganger_kjørt = 0 
 while True:
+    antall_ganger_kjørt += 1
     try:
         updates = int(hent_linjer("conf/etc.conf",1,True))
         amount_of_lines = int(hent_linjer("conf/etc.conf",3,True))
         print(f"program updates every {updates} sek og writes lines {amount_of_lines} to file")
-        print(f"start time {dato_og_tid()}")
+        print(f"start time {dato_og_tid()}") if antall_ganger_kjørt < 0 else None
     except:
-        print(f"start time {dato_og_tid()}")
+        print(f"start time {dato_og_tid()}") if antall_ganger_kjørt < 0 else None
         updates = 5
         amount_of_lines = 1
         print("etc.conf was not found")
