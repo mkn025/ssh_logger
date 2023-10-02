0af440ecbcf398048eb63b1de1b62f0f90870f7a
mkn025 <martin.elde.knutsen@gmail.com>
2023-10-02 22:19:38 +0200

    maIn

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import time
 logg_tester = None
 antall_ganger_kjørt = 0 
 while True:
-    antall_ganger_kjørt += 1
     try:
         updates = int(hent_linjer("conf/etc.conf",1,True))
         amount_of_lines = int(hent_linjer("conf/etc.conf",3,True))
         print(f"program updates every {updates} sek og writes lines {amount_of_lines} to file")
         print(f"start time {dato_og_tid()}") and append_to_file("logg/logg.txt", dato_og_tid()) if antall_ganger_kjørt < 0 else None
     except:
-        print(f"start time {dato_og_tid()}") if antall_ganger_kjørt < 0 else None
+        print(f"start time {dato_og_tid()}") and append_to_file("logg/logg.txt", dato_og_tid()) if antall_ganger_kjørt < 0 else None
         updates = 5
         amount_of_lines = 1
         print("etc.conf was not found")
@@ -23,6 +22,8 @@ while True:
             append_to_file("logg/logg.txt",n)
             ip_adresse = find_ip_addresses(n)
             append_to_file("logg/logg_ip.txt",f"ip adresse: {ip_adresse[0]} <==> {dato_og_tid()}") if len(ip_adresse) > 0 else None
+        antall_ganger_kjørt += 1
+            
             
     logg_tester = logg
     time.sleep(updates)
